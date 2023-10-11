2d9a148cb0374ef60a3e1060e96a2b5d71a266ff
Elaidzha1940 <eli080293@gmail.com>
2023-10-11 16:18:22 +0300

    SecurePasswordField

---
+++ SecurePasswordField/SecurePasswordFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecurePasswordFieldApp.swift
+//  SecurePasswordField
+//
+//  Created by Elaidzha Shchukin on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SecurePasswordFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
