140113553a5f6825d481a167fdafef23c76bf242
Elaidzha1940 <eli080293@gmail.com>
2023-10-13 18:21:11 +0300

    Updated MARKs

--- SecurePasswordField/ContentView.swift
+++ SecurePasswordField/ContentView.swift
@@ -10,20 +10,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Properties
     @State private var password = "password"
     @State private var showPassword = true
     
+    // MARK: Body
+    
+    /*
+     Fix RoundedRectangle
+     */
     var body: some View {
         
-        VStack {
-        
-            RoundedRectangle(cornerRadius: 50)
-                .frame(width: 300, height: 70, alignment: .center)
-        }
+        VStack { // Start: VS
+            
+            // foreground
+            foregroundLayer
+            
+        } // END: VS
+        // background
         .preferredColorScheme(.dark)
+    }
+    
+    private var foregroundLayer: some View {
+        RoundedRectangle(cornerRadius: 50)
+            .frame(width: 300, height: 70, alignment: .center)
+        
     }
 }
 
+// MARK: Preview
 #Preview {
     ContentView()
 }
