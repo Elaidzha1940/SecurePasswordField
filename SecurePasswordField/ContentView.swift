50012b68714f3a248fc016ee0d2f50e7fa2ce020
Elaidzha1940 <eli080293@gmail.com>
2023-10-13 18:51:59 +0300

    Add TextField

--- SecurePasswordField/ContentView.swift
+++ SecurePasswordField/ContentView.swift
@@ -22,38 +22,38 @@ struct ContentView: View {
      */
     var body: some View {
         
-        VStack { // Start: VS
+        ZStack { // Start: VS
             
             // foreground
-            foregroundLayer
+            RoundedRectangle(cornerRadius: 50)
+                .frame(width: 300, height: 70, alignment: .center)
+                .foregroundColor(.gray)
             
+            HStack {
+                if !showPassword {
+                    Spacer()
+                }
+                RoundedRectangle(cornerRadius: showPassword ? 25 : 50)
+                    .frame(width: showPassword ? 300 : 50, height: showPassword ? 75 : 50, alignment: .center)
+                    .animation(.linear(duration: 0.2))
+                    .padding(.trailing, showPassword ? 0 : 12)
+            }
+            
+            HStack {
+                if !showPassword {
+                    TextField("password", text: $password)
+                        .font(.system(size: 15, weight: .semibold, design: .serif))
+                        .foregroundColor(.black)
+                }
+            }
         } // END: VS
         // background
         .preferredColorScheme(.dark)
     }
-    
-    private var foregroundLayer: some View {
-        RoundedRectangle(cornerRadius: 50)
-            .frame(width: 300, height: 70, alignment: .center)
-            .foregroundColor(.gray)
-        
-        return HStack {
-            if !showPassword {
-                Spacer()
-            }
-            RoundedRectangle(cornerRadius: showPassword ? 25 : 50)
-                .frame(width: showPassword ? 300 : 50, height: showPassword ? 75 : 50, alignment: .center)
-                .animation(.linear(duration: 0.2))
-                .padding(.trailing, showPassword ? 0 : 12)
-        }
-        
-        HStack {
-            
-        }
-    }
 }
-
 // MARK: Preview
 #Preview {
     ContentView()
 }
+
+
